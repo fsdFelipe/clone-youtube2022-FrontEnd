a106700ee2c59ea571105c519dc22e7d1feb8660
fsdFelipe <devfsdfelipe@gmail.com>
2022-12-06 18:02:32 -0300

    Design Pagina de Video

--- src/page/Home/Video.js
+++ src/page/Home/Video.js
@@ -1,8 +1,153 @@
 import React from "react";
+import styled from "styled-components";
+import ThumbUpOutlinedIcon from '@mui/icons-material/ThumbUpAltOutlined'
+import ThumbDownOffAltOutlinedIcon from '@mui/icons-material/ThumbDownOffAltOutlined'
+import ReplyOutlinedIcon from '@mui/icons-material/ReplyOutlined'
+import AddTaskOutlinedIcon from '@mui/icons-material/AddTaskOutlined'
+
+const Container = styled.div`
+display: flex;
+gap: 24px;
+`;
+
+const Content = styled.div`
+flex: 5;
+`;
+
+const VideoWrapper = styled.div`
+`;
+
+const Title = styled.h1`
+font-size: 18px;
+font-weight: 400;
+margin-top: 20px;
+margin-bottom: 10px;
+color: ${({theme}) => theme.text};
+`;
+
+const Details = styled.div`
+display: flex;
+align-items: center;
+justify-content: space-between;
+`;
+
+const Info = styled.span`
+color: ${({theme}) => theme.textSoft}
+`;
+
+const Buttons = styled.div`
+display: flex;
+gap: 20px;
+color: ${({theme}) => theme.text}
+`;
+
+const Button = styled.div`
+display: flex;
+align-items: center;
+gap: 5px;
+cursor: pointer;
+`;
+
+const Hr = styled.hr`
+border: 0.5px solid ${({ theme}) => theme.soft};
+margin-top: 15px 0px;
+`;
+
+const Recomendation = styled.div`
+flex: 2;
+`;
+
+const Channel = styled.div`
+display: flex;
+justify-content: space-between;
+`;
+
+const ChannelInfo = styled.div`
+display: flex;
+gap: 20;
+`;
+
+const Image = styled.img`
+width: 50px;
+height: 50px;
+border-radius: 50%;
+`;
+
+const ChannelDetail = styled.div`
+display: flex;
+flex-direction: column;
+color: ${({theme}) => theme.text};
+margin-left: 20px;
+`;
+const ChannelName = styled.span`
+font-weight: 500;
+`;
+const ChannelCounter = styled.span`
+margin-top: 5px;
+margin-bottom: 20px;
+color: ${({theme}) => theme.textSoft};
+font-size: 12px;
+`;
+const Description = styled.p`
+font-size: 14px;
+`;
+
+const Subscribe = styled.button`
+background-color: #cc1a00;
+font-weight: 500;
+color: white;
+border: none;
+border-radius: 3px;
+height: max-content;
+padding 10px 20px;
+cursor: pointer;
+`;
 
 const Video = () => {
     return(
-        <div>Video</div>
+        <Container>
+            <Content>
+            <VideoWrapper>
+                <iframe width="100%" height="720" 
+                src="https://www.youtube.com/embed/yIaXoop8gl4" 
+                title="YouTube video player" frameborder="0" 
+                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" 
+                allowfullscreen>
+                </iframe>
+            </VideoWrapper>
+            <Title>Video Teste</Title>
+            <Details>
+                <Info>54321 views - dec 06, 2022</Info>
+                <Buttons>
+                    <Button><ThumbUpOutlinedIcon /> 1234</Button>
+                    <Button>
+                        <ThumbDownOffAltOutlinedIcon /> Dislike
+                    </Button>
+                    <Button>
+                        <ReplyOutlinedIcon /> Share
+                    </Button>
+                    <Button>
+                        <AddTaskOutlinedIcon /> Save
+                    </Button>
+                </Buttons>
+            </Details>
+            <Hr />
+            <Channel>
+                <ChannelInfo>
+                    <Image src="https://upload.wikimedia.org/wikipedia/commons/a/a3/Black-Magic-Big-Boy.jpg" />
+                    <ChannelDetail>
+                        <ChannelName>Felipe</ChannelName>
+                        <ChannelCounter>1 subscriber</ChannelCounter>
+                        <Description> Descrição do canal apenas para ter um texto mesmo.
+                            Escrevendo qualquer coisa, para ver o resultado na pagina de video
+                        </Description>
+                    </ChannelDetail>
+                </ChannelInfo>
+                <Subscribe>SUBSCRIBE</Subscribe>
+            </Channel>
+            </Content>
+            <Recomendation>Recomendation</Recomendation>
+        </Container>
     )
 }
 
