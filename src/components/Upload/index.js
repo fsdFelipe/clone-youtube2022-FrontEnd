e396da0c11e9ef3d5f86148ebcf3a172b5c7754d
fsdFelipe <devfsdfelipe@gmail.com>
2022-12-12 18:29:33 -0300

    Fazendo upload de video title e desc para fireba Storage

--- src/components/Upload/index.js
+++ src/components/Upload/index.js
@@ -2,6 +2,8 @@ import React, { useEffect, useState } from "react";
 import styled from "styled-components";
 import { getStorage, ref, uploadBytesResumable, getDownloadURL } from "firebase/storage"
 import app from '../../firebase'
+import axios from "axios"
+import { useNavigate } from "react-router-dom";
 
 const Container = styled.div`
   width: 100%;
@@ -64,110 +66,121 @@ const Label = styled.label`
   font-size: 14px;
 `;
 
-const Upload = ({setOpen}) =>{
-    const [img, setImg] = useState(undefined)
-    const [video, setVideo] = useState(undefined)
-    const [imgPerc, setImgPerc] = useState(0)
-    const [videoPerc, setVideoPerc] = useState(0)
-    const [inputs, setInputs] = useState("")
-    const [tags, setTags] = useState([])
+const Upload = ({ setOpen }) => {
+  const [img, setImg] = useState(undefined);
+  const [video, setVideo] = useState(undefined);
+  const [imgPerc, setImgPerc] = useState(0);
+  const [videoPerc, setVideoPerc] = useState(0);
+  const [inputs, setInputs] = useState({});
+  const [tags, setTags] = useState([]);
 
-    const handleChange = (e) =>{
-        setInputs((prev) =>{
-            return { ...prev, [e.target.name] : e.target.value};
-        })
-    }
+  const navigate = useNavigate()
 
-    const handleTags = (e) => {
+  const handleChange = (e) => {
+    setInputs((prev) => {
+      return { ...prev, [e.target.name]: e.target.value };
+    });
+  };
+
+  const handleTags = (e) => {
     setTags(e.target.value.split(","));
   };
 
-  const upLoadFile = (file, urlType) =>{
+  const uploadFile = (file, urlType) => {
     const storage = getStorage(app);
-    const fileName = new Date().getTime() + file.name
+    const fileName = new Date().getTime() + file.name;
     const storageRef = ref(storage, fileName);
     const uploadTask = uploadBytesResumable(storageRef, file);
 
-    // Listen for state changes, errors, and completion of the upload.
-uploadTask.on('state_changed',
-  (snapshot) => {
-    // Get task progress, including the number of bytes uploaded and the total number of bytes to be uploaded
-    const progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
-    urlType === "imgUrl" ? setImgPerc(progress) : setVideoPerc(progress)
-    switch (snapshot.state) {
-      case 'paused':
-        console.log('Upload is paused');
-        break;
-      case 'running':
-        console.log('Upload is running');
-        break;
-        default:
-        break;
-    }
-  }, 
-  (error) => {
-    // A full list of error codes is available at
-    // https://firebase.google.com/docs/storage/web/handle-errors
-    switch (error.code) {
-      case 'storage/unauthorized':
-        // User doesn't have permission to access the object
-        break;
-      case 'storage/canceled':
-        // User canceled the upload
-        break;
-
-      // ...
-
-      case 'storage/unknown':
-        // Unknown error occurred, inspect error.serverResponse
-        break;
-    }
-  }, 
-  () => {
-    // Upload completed successfully, now we can get the download URL
-    getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
-       setInputs((prev) =>{
-            return { ...prev, [urlType]: downloadURL};
-        })
-    });
-  }
-);
-
-  }
+    uploadTask.on(
+      "state_changed",
+      (snapshot) => {
+        const progress =
+          (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+        urlType === "imgUrl" ? setImgPerc(Math.round(progress)) : setVideoPerc(Math.round(progress));
+        switch (snapshot.state) {
+          case "paused":
+            console.log("Upload is paused");
+            break;
+          case "running":
+            console.log("Upload is running");
+            break;
+          default:
+            break;
+        }
+      },
+      (error) => {},
+      () => {
+        getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
+          setInputs((prev) => {
+            return { ...prev, [urlType]: downloadURL };
+          });
+        });
+      }
+    );
+  };
 
   useEffect(() => {
-   video && upLoadFile(video, "videoUrl")},
-     [video]
-     )
-  useEffect(() => {
-   img && upLoadFile(img, "imgUrl")},
-     [img]
-     )
+    video && uploadFile(video , "videoUrl");
+  }, [video]);
 
-    return(
-        <Container>
-            <Wrapper>
-                <Close onClick={() => setOpen(false)}>X</Close>
-                <Title>UpLoad a new Video</Title>
-                <Label>Video:</Label>
-                {videoPerc > 0 ? ("Uploading: " + videoPerc) : (
-                <Input type="file" accept="video/*" onChange={e => setVideo(e.target.files[0])}/>
-                )}
-                <Input type="text" placeholder="Title"
-                name="title"
-                 onChange={handleChange}/>
-                <Desc placeholder="Description" rows={8}
-                name="desc"
-                onChange={handleChange}/>
-                <Input type="text" placeholder="Separeted with commas" onChange={handleTags} />
-                <Label>Image:</Label>
-                {imgPerc > 0 ? ("Uploading: " + imgPerc) : (
-                <Input type="file" accept="image/*" onChange={e => setImg(e.target.files[0])}/>
-                )}
-                <Button>Upload</Button>
-            </Wrapper>
-        </Container>
-    )
-}
+  useEffect(() => {
+    img && uploadFile(img, "imgUrl");
+  }, [img]);
+
+  const handleUpload = async (e)=>{
+    e.preventDefault();
+    const res = await axios.post("/videos", {...inputs, tags})
+    setOpen(false)
+    res.status===200 && navigate(`/video/${res.data._id}`)
+  }
+
+  return (
+    <Container>
+      <Wrapper>
+        <Close onClick={() => setOpen(false)}>X</Close>
+        <Title>Upload a New Video</Title>
+        <Label>Video:</Label>
+        {videoPerc > 0 ? (
+          "Uploading:" + videoPerc
+        ) : (
+          <Input
+            type="file"
+            accept="video/*"
+            onChange={(e) => setVideo(e.target.files[0])}
+          />
+        )}
+        <Input
+          type="text"
+          placeholder="Title"
+          name="title"
+          onChange={handleChange}
+        />
+        <Desc
+          placeholder="Description"
+          name="desc"
+          rows={8}
+          onChange={handleChange}
+        />
+        <Input
+          type="text"
+          placeholder="Separate the tags with commas."
+          onChance={handleTags}
+        />
+        <Label>Image:</Label>
+        {imgPerc > 0 ? (
+          "Uploading:" + imgPerc + "%"
+        ) : (
+          <Input
+            type="file"
+            accept="image/*"
+            onChange={(e) => setImg(e.target.files[0])}
+          />
+        )}
+        <Button onClick={handleUpload}>Upload</Button>
+      </Wrapper>
+    </Container>
+  );
+};
 
 export default Upload
